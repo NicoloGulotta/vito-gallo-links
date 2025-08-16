deb8ffde8447dda6cccaa89d82a775b8dde6d424
Nicolò Gulotta <gulottanico1998@gmail.com>
2025-08-16 19:12:34 +0200

    versione marco

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { faFacebookF, faInstagram, faTiktok } from '@fortawesome/free-brands-svg-icons';
-import logo from './assets/291978694_465978512197506_768194558146679677_n.jpg';
+import logo from './assets/logo.png';
 
 function App() {
   return (
@@ -20,7 +20,7 @@ function App() {
             <h1 className="display-4 fw-bold">Lascia una recensione</h1>
           </Col>
           <Col xs={12} md={6} lg={4} className="mb-3 text-center">
-            <Button className="btn-light btn-lg btn-block btn-social btn-google" href="https://www.google.com/maps/place/Vito+Gallo+Parrucchieri/@37.5096354,13.0742367,17z/data=!4m8!3m7!1s0x131a47b25aec0b4f:0xcd4736033c716f1d!8m2!3d37.5096312!4d13.0768116!9m1!1b1!16s%2Fg%2F11h_wn_sw8?entry=ttu">
+            <Button className="btn-light btn-lg btn-block btn-social btn-google" href="">
               <FontAwesomeIcon icon={faGoogle} className="me-2" /> Google
             </Button>
           </Col>
@@ -31,30 +31,40 @@ function App() {
             <h1 className="display-4 fw-bold">Seguici sui social</h1>
           </Col>
           <Col xs={12} md={6} lg={4} className="mb-3 text-center">
-            <Button className="btn-primary btn-lg btn-block btn-social btn-facebook" href="https://www.facebook.com/VGparrucchiere/?locale=it_IT">
+            <Button className="btn-primary btn-lg btn-block btn-social btn-facebook" href="https://www.facebook.com/share/1AtWhhi6YS/">
               <FontAwesomeIcon icon={faFacebookF} className="me-2" /> Facebook
             </Button>
           </Col>
           <Col xs={12} md={6} lg={4} className="mb-3 text-center">
-            <Button className="btn-danger btn-lg btn-block btn-social btn-social btn-instagram" href="https://www.instagram.com/vitogalloparrucchieri/">
+            <Button className="btn-danger btn-lg btn-block btn-social btn-social btn-instagram" href="https://www.instagram.com/marco_sw_24/profilecard/?igsh=MWVxa3h6NWF4aHhmcg==">
+              <FontAwesomeIcon icon={faInstagram} className="me-2" /> Instagram personale
+            </Button>
+          </Col>
+          <Col xs={12} md={6} lg={4} className="mb-3 text-center">
+            <Button className="btn-danger btn-lg btn-block btn-social btn-social btn-instagram" href="https://www.instagram.com/mc.calisthenics?igsh=enZhbmN3aTk2dmlj">
               <FontAwesomeIcon icon={faInstagram} className="me-2" /> Instagram
             </Button>
           </Col>
           <Col xs={12} md={6} lg={4} className="mb-3 text-center">
-            <Button className="btn-dark btn-lg btn-block btn-social btn-tiktok" href="https://www.tiktok.com/place/Parrucchiere-Vito-Gallo-21568295496296882">
+            <Button className="btn-dark btn-lg btn-block btn-social btn-tiktok" href="">
               <FontAwesomeIcon icon={faTiktok} className="me-2" /> TikTok
+            </Button>
+          </Col>
+          <Col xs={12} md={6} lg={4} className="mb-3 text-center">
+            <Button className="btn-success btn-lg btn-block btn-social btn-whatsapp" href="https://wa.me/qr/WM5AUQOJLFLMO1">
+              <FontAwesomeIcon icon={faTiktok} className="me-2" /> Whatsapp
             </Button>
           </Col>
         </Row>
 
-        {/* <Row className="justify-content-center">
+        <Row className="justify-content-center">
           <Col xs={12} md={6} lg={4} className="mb-3 text-center">
             <Button className="btn-outline-light btn-lg btn-block btn-social disabled" href="#">
-              <h2 className="mb-0">Sito web</h2>
+              <h2 className="mb-0">App web</h2>
               <span className="small">(coming soon)</span>
             </Button>
           </Col>
-        </Row> */}
+        </Row>
       </Container>
     </div>
   );
